8bdc488fcb725eb9fcb46873edabcb56cc0bf109
Keegan Green <keegan.green@bluwave-ai.com>
2024-06-22 20:03:43 -0400

    removed visualizer_environment.py

--- src/environments/__init__.py
+++ src/environments/__init__.py
@@ -1,7 +1,1 @@
 from .environment import BaseEnvironment, Environment, EnvironmentConfig
-from .visualizer_environment import VisualizerEnvironment
-
-ENVIRONMENT_CLASSES = {
-    "Environment": Environment,
-    "VisualizerEnvironment": VisualizerEnvironment,
-}
