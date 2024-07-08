63371c6877a9fde40b482848000255263e0daa05
Keegan Green <keegan.green@bluwave-ai.com>
2024-07-07 23:07:15 -0400

    video preview

--- combine_manim_videos.py
+++ combine_manim_videos.py
@@ -3,12 +3,14 @@ from pathlib import Path
 
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 
-FNAMES = ["Intro", "Video"]
-
 parser = ArgumentParser()
 parser.add_argument("--work-dir")
 args = parser.parse_args()
 
-clips = [VideoFileClip(str(Path(args.work_dir, f"{fname}.mp4"))) for fname in FNAMES]
+clips = [
+    VideoFileClip(str(Path(args.work_dir, f"Video.mp4"))).subclip("00:00:53", "00:00:54"),
+    VideoFileClip(str(Path(args.work_dir, f"Intro.mp4"))),
+    VideoFileClip(str(Path(args.work_dir, f"Video.mp4"))),
+]
 final = concatenate_videoclips(clips)
 final.write_videofile(str(Path(args.work_dir, "final.mp4")))
